a8d2e6c287537ddd487fa71408b3f56b28cec218
Valentin Cladel <valentin.cladel@etudiant.univ-reims.fr>
2023-12-04 10:59:49 +0100

    TP3 - Jusqu'au point 13

--- src/movies-api.js
+++ src/movies-api.js
@@ -4,5 +4,25 @@ export function getAllMovies()
 {
     return fetch(`${API_URL}/movies`)
     .then(
-        (response)=> response.json());
+        (response)=> extractCollectionAndPagination(response));
+}
+
+export function posterUrl(imagePath,size='original')
+{
+    return `${API_URL}${imagePath}/${size}`;
+}
+
+export function extractPaginationFromHeaders(response)
+{
+    const obj = {}
+    obj.current= parseInt(response.headers.get('Pagination-Current-Page'));
+    obj.last= parseInt(response.headers.get('Pagination-Last-Page'));
+    return obj;
+}
+
+export function extractCollectionAndPagination(response)
+{
+    return response.json().then(
+        (collection)=>{return {collection:collection,pagination:extractPaginationFromHeaders(response)}}
+    )
 }
--- src/movies-ui.js
+++ src/movies-ui.js
@@ -1,25 +1,74 @@
-import {getAllMovies} from "./movies-api";
+import {getAllMovies, posterUrl} from "./movies-api";
 
 export function createMovieElt(movieData)
 {
+    const titre = movieData.title;
     const artiElt = document.createElement("article");
     artiElt.className = "movie-item";
     const divElt = document.createElement("div");
     divElt.className = "movie-item__info";
     const titElt = document.createElement("div");
     titElt.className = "movie-item__title";
+    const imElt = document.createElement("img");
+    imElt.className = 'movie-item__poster'
 
-    titElt.innerHTML=`${movieData.title}`;
+    titElt.innerHTML=titre;
+    imElt.alt=`poster of \'${titre}\'`;
+    imElt.src= posterUrl(`${movieData.poster}`,'medium');
     divElt.appendChild(titElt);
     artiElt.appendChild(divElt);
+    artiElt.appendChild(imElt);
     return artiElt;
 }
 
 export function updateMoviesElt()
 {
     const tab = getAllMovies();
-    const arti = document.querySelector(".movies-list")
-    tab.then((data)=> {data.forEach((movie)=>{
+    const arti = document.querySelector(".movies-list");
+    tab.then((data)=> {data.collection.forEach((movie)=>{
         arti.appendChild(createMovieElt(movie))
     })})
+}
+
+export function createPaginationButtonElt(materialIcon, isDisabled, page)
+{
+    const but = document.createElement("button");
+    const icone = document.createElement("span");
+    icone.className="material-symbols-outlined";
+    icone.innerHTML=materialIcon;
+    but.disabled=isDisabled;
+    but.type="button";
+    but.className="button";
+    but.formAction=`/${page}`;
+    but.appendChild(icone);
+    but.addEventListener("click", updateMoviesElt);
+
+    return but;
+}
+
+export function emptyElt(elt)
+{
+    while (elt.hasChildNodes()){
+        elt.removeChild(elt.firstChild)
+    }
+}
+
+export function updatePaginationElt(pagination)
+{
+    if(pagination.last >1){
+        const but1 = createPaginationButtonElt('first_page',pagination.current===1,1);
+        const but2 = createPaginationButtonElt('navigate_before',pagination.current===1, pagination.current-1);
+        const but3 = createPaginationButtonElt('navigate_next',pagination.current===pagination.last,pagination.current+1);
+        const but4 = createPaginationButtonElt('last_page',pagination.current===pagination.last,pagination.last);
+        const pagi = document.createElement("span");
+        pagi.className="pagination__info";
+        pagi.innerHTML= `${pagination.current}/${pagination.last}`;
+
+        const navi = document.querySelector("nav.pagination");
+        navi.appendChild(but1);
+        navi.appendChild(but2);
+        navi.appendChild(pagi);
+        navi.appendChild(but3);
+        navi.appendChild(but4);
+    }
 }
